0c42c1ab1d37bdd80de5fdcab1b7841c7a166d59
Adrian <Zilibdev@gmail.com>
2019-12-05 00:30:14 +0100

    Make corridor infinity, it is spawning every second!

---
+++ EndlessRunner/Source/EndlessRunner/Private/SpawnableCorridor.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnableCorridor.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
+#include "Engine/StaticMesh.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values
+ASpawnableCorridor::ASpawnableCorridor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+void ASpawnableCorridor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorldTimerManager().SetTimer(Handler, this, &ASpawnableCorridor::SpawnCorridor, 1.f, false);
+}
+
+void ASpawnableCorridor::SpawnCorridor()
+{
+	if (CorridorToSpawn == nullptr) { return; }
+	UE_LOG(LogTemp, Warning, TEXT("%s: Translation"), *GetTransform().ToString());
+	FVector NewLocation = FVector(
+		GetTransform().GetLocation().X + CorridorWidth,
+		GetTransform().GetLocation().Y,
+		GetTransform().GetLocation().Z
+	);
+
+	FTransform x = FTransform(
+		GetTransform().GetRotation(),
+		NewLocation,
+		GetTransform().GetScale3D()
+	);
+	
+	auto NewCorridor = GetWorld()->SpawnActor<ASpawnableCorridor>(
+		CorridorToSpawn,
+		x
+		);
+}
---
+++ EndlessRunner/Source/EndlessRunner/Public/SpawnableCorridor.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TimerManager.h"
+#include "GameFramework/Actor.h"
+#include "SpawnableCorridor.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class ENDLESSRUNNER_API ASpawnableCorridor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnableCorridor();
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	TSubclassOf<ASpawnableCorridor> CorridorToSpawn { nullptr };
+
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	float CorridorWidth{ 600.0f };
+
+	FTimerHandle Handler;
+
+	void SpawnCorridor();
+};

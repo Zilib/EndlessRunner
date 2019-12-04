999d67da74969d2a219f89cf3c9c0c0d85cf45fe
Adrian <Zilibdev@gmail.com>
2019-12-04 17:14:00 +0100

    Create new map, change default names and project settings

--- EndlessRunner/Source/EndlessRunner/EndlessRunnerGameMode.cpp
+++ EndlessRunner/Source/EndlessRunner/EndlessRunnerGameMode.cpp
@@ -6,7 +6,7 @@
 AEndlessRunnerGameMode::AEndlessRunnerGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/RunnerCharacter_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
